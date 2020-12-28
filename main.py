0449791fd4e17792e533ff273f5a46891399c96b
Bilaal Ahmad <bmahmad2@illinois.edu>
2020-12-28 14:33:08 -0800

    integrated scraper with main, calculates predicted total

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import scraper
 
 
 def main():
-    df = pd.read_csv(r'/Users/bilaalahmad/Desktop/all_nba_data.csv', encoding='utf8')
-    print(df)
+    data = pd.read_csv(r'/Users/bilaalahmad/Desktop/clean_nba_data.csv', encoding='utf8')
+    pd.to_numeric(data['PV'])
+    pd.to_numeric(data['PH'])
+    matchups = scraper.main()
+
+    for index, matchup in enumerate(matchups):
+        team1 = str(matchups[index][0])
+        team2 = str(matchups[index][1])
+        total = int(matchups[index][2])
+        relevant_games = data[((data['Visitor'] == team1) | (data['Home'] == team1))
+                                  & ((data['Visitor'] == team2) | (data['Home'] == team2))]
+        avg_pts = (relevant_games['PV'].sum() + relevant_games['PH'].sum()) / relevant_games['PV'].count()
+        avg_pts = round(avg_pts, 3)
+        diff = abs(avg_pts - total)
+
+        print(f'GAME: {team1} vs {team2}, TOTAL: {total}, PREDICTED: {avg_pts}, DIFF: {diff}')
 
 
 if __name__ == '__main__':
     main()
+
+
--- scraper.py
+++ scraper.py
@@ -2,20 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://mybookie.ag/sportsbook/nba/'
-page = requests.get(url)
+# returns list of [TEAM, TEAM, TOTAL] lists
+def main():
+    url = 'https://mybookie.ag/sportsbook/nba/'
+    page = requests.get(url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id='accordionBets3')
+    results = soup.find(id='accordionBets3')
 
-# iterable containing main games
-games = results.find_all('div', class_='game-line py-3')
+    # iterable containing main games
+    games = results.find_all('div', class_='game-line py-3')
+    info = []
 
-for game in games:
-    # iterable containing buttons for each game
-    all_game_buttons = game.find_all('button', class_='lines-odds')
-    #
-    game_info = all_game_buttons[5]
-    print(game_info)
-    print('------------new game-------------')
+    for index, game in enumerate(games, start=1):
+        # iterable containing buttons for each game
+        all_game_buttons = game.find_all('button', class_='lines-odds')
+        # HTML string for button with over under + teams
+        game_info = str(all_game_buttons[5])
+        # locate team names and total
+        team1 = game_info[game_info.find('data-team') + 11: game_info.find('data-team-vs') - 2]
+        team2 = game_info[game_info.find('data-team-vs') + 14: game_info.find('data-wager') - 2]
+        total = int(game_info[game_info.find('U ') + 2: game_info.find('U ') + 5])
+        # print(f'GAME {index}: {team2} @ {team1}, TOTAL: {total}')
+        info.append([team1, team2, total])
+
+    return info
+
+
+if __name__ == '__main__':
+    # print the result (list of list) of scraper main
+    print(main())
